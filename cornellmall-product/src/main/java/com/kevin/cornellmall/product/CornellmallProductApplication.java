3efb345f566c57b879d79fb458a78bc32bf71f97
Haowen Jiang <hj323@cornell.edu>
2022-02-20 00:20:50 -0500

    CRUD Test Finished

--- cornellmall-product/src/main/java/com/kevin/cornellmall/product/CornellmallProductApplication.java
+++ cornellmall-product/src/main/java/com/kevin/cornellmall/product/CornellmallProductApplication.java
@@ -1,8 +1,10 @@
 package com.kevin.cornellmall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.kevin.cornellmall.product.dao")
 @SpringBootApplication
 public class CornellmallProductApplication {
 
--- cornellmall-product/src/test/java/com/kevin/cornellmall/product/CornellmallProductApplicationTests.java
+++ cornellmall-product/src/test/java/com/kevin/cornellmall/product/CornellmallProductApplicationTests.java
@@ -1,13 +1,33 @@
 package com.kevin.cornellmall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kevin.cornellmall.product.entity.BrandEntity;
+import com.kevin.cornellmall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CornellmallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("华为nb");
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功");
+//        brandService.updateById(brandEntity);
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach((item) -> {
+            System.out.println(item);
+        });
     }
 
 }
